7cee98dfbd20cf4475ae19ee15ebe0334fa4daa8
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-05-30 09:39:11 -0300

    react-testes

--- appreact/src/App.jsx
+++ appreact/src/App.jsx
@@ -1,31 +1,15 @@
-import React from "react";
-import Produto from "./Produto";
+import React from 'react';
+import Produto from './Produto';
+import { GlobalStorage } from './GlobalContext';
+import Limpar from "./Limpar";
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
-
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem("produto");
-    if (produtoLocal !== "null") setProduto(produtoLocal);
-  }, []);
-
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem("produto", produto);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
-  }
-
   return (
-    <div>
-      <h1>Preferência: {produto}</h1>
-      <button onClick={handleClick} style={{ marginRight: "1em" }}>
-        Notebook
-      </button>
-      <button onClick={handleClick}>Smartphone</button>
-      <Produto produto={produto} />
-    </div>
+    <GlobalStorage>
+      <Produto />
+      <Limpar />
+    </GlobalStorage>
   );
 };
+
 export default App;
