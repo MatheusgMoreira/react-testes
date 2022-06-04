c2104b81c3b7b500a805d2235b5593566a52b1ae
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-06-04 16:45:32 -0300

    react-testes

--- appreact/src/App.jsx
+++ appreact/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Produto from './Produto';
-import { GlobalStorage } from './GlobalContext';
-import Limpar from "./Limpar";
+import useFetch from './useFetch';
 
 const App = () => {
-  return (
-    <GlobalStorage>
-      <Produto />
-      <Limpar />
-    </GlobalStorage>
-  );
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    request('https://ranekapi.origamid.dev/json/api/produto/notebook');
+  }, [request]);
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data) return <div>{data.nome}</div>;
+  else return null;
 };
 
 export default App;
